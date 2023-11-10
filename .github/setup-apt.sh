fcc434dec00251fd9f138d65eb00cd97ec6c6a50
Ludovic Henry <git@ludovic.dev>
2023-11-10 17:17:05 +0000

    fixup! Add centos:7 configuration for releases

--- .github/setup-apt.sh
+++ .github/setup-apt.sh
@@ -7,5 +7,5 @@ dpkg --add-architecture i386
 apt update
 apt install -y autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev \
             libgmp-dev gawk build-essential bison flex texinfo gperf libtool \
-            patchutils bc zlib1g-dev libexpat-dev git ninja-build cmake libglib2.0-dev expect \
-            device-tree-compiler
+            patchutils bc zlib1g-dev libexpat-dev git ninja-build libglib2.0-dev expect \
+            device-tree-compiler wget
--- .github/setup-yum.sh
+++ .github/setup-yum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 # install OS prerequisites
-yum install -y autoconf automake python3 libmpc-devel mpfr-devel gmp-devel gawk  bison flex texinfo patchutils gcc gcc-c++ zlib-devel expat-devel make cmake
+yum install -y autoconf automake python3 libmpc-devel mpfr-devel gmp-devel gawk  bison flex texinfo patchutils gcc gcc-c++ zlib-devel expat-devel make wget
 
 # install devtoolset-11
 yum install -y centos-release-scl
